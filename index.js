ee45f53d962e6981dd017ea0095948783a970720
Zhirrr <75620279+Zhirrr@users.noreply.github.com>
2021-01-09 16:22:38 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const waifuScrape = require('./src/islamic')
+const fs = require('fs')
+const path = require('path')
+const chalk = require('chalk')
+const outputFile = path.join(__dirname, 'data/', 'data.json')
+const baseUrl = "http://islam.nu.or.id"
